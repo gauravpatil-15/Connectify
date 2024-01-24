a4a351a4ba46231a728a75389606bbe868b35a15
gauravpatil-15 <gauravpatil.rajendra@gmail.com>
2024-01-25 00:35:39 +0530

    some modifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,19 @@ import React, { useContext } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
 import { AuthContext } from '../context/AuthContext';
+import smiley from "../images/smiley.png"
 
 const Navbar = () => {
 
     const { currentUser } = useContext(AuthContext);
 
+    // console.log(currentUser.photoURL);
+
     return (
         <div className='navbar'>
             <span className="logo">Connectify</span>
             <div className="user">
-                <img src={currentUser.photoURL} alt="" />
+                <img src={currentUser.photoURL ? currentUser.photoURL : smiley} alt="" />
                 <span>{currentUser.displayName}</span>
                 <button onClick={() => signOut(auth)}>logout</button>
             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,6 +9,7 @@ import { Link, useNavigate } from "react-router-dom";
 const Register = () => {
 
     const [err, setErr] = useState(false);
+    const [img, setImg] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -19,39 +20,43 @@ const Register = () => {
         const file = e.target[3].files[0];
         const date = new Date().getTime();
 
+        console.log(file);
+
         try {
             //Create user
             const res = await createUserWithEmailAndPassword(auth, email, password);
 
             //Create a unique image name
             const storageRef = ref(storage, `${displayName + date}`);
-
-            await uploadBytesResumable(storageRef, file).then(() => {
-                getDownloadURL(storageRef).then(async (downloadURL) => {
-                    try {
-                        //Update profile
-                        await updateProfile(res.user, {
-                            displayName,
-                            photoURL: downloadURL,
-                        });
-                        //create user on firestore
-                        await setDoc(doc(db, "users", res.user.uid), {
-                            uid: res.user.uid,
-                            displayName,
-                            email,
-                            photoURL: downloadURL,
-                        });
-
-                        //create empty user chats on firestore
-                        await setDoc(doc(db, "userChats", res.user.uid), {});
-                        navigate("/");
-                        
-                    } catch (err) {
-                        console.log(err);
-                        setErr(true);
-                    }
+            if(file){
+                await uploadBytesResumable(storageRef, file).then(() => {
+                    getDownloadURL(storageRef).then(async (downloadURL) => {
+                        setImg(downloadURL);
+                    });
                 });
-            });
+            }
+            try {
+                //Update profile
+                await updateProfile(res.user, {
+                    displayName,
+                    photoURL: img,
+                });
+                //create user on firestore
+                await setDoc(doc(db, "users", res.user.uid), {
+                    uid: res.user.uid,
+                    displayName,
+                    email,
+                    photoURL: img,
+                });
+
+                //create empty user chats on firestore
+                await setDoc(doc(db, "userChats", res.user.uid), {});
+                navigate("/");
+                
+            } catch (err) {
+                console.log(err);
+                setErr(true);
+            }
         } catch (err) {
             setErr(true);
         }
@@ -66,7 +71,7 @@ const Register = () => {
                     <input type="text" placeholder="user name" />
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" autoComplete="on" />
-                    <input style={{ display: "none" }} type="file" id="file" />
+                    <input style={{ display: "none" }} type="file" id="file" alt="" />
                     <label htmlFor="file">
                         <img src={Add} alt="" />
                         <span>Add an Avatar</span>
